23575e5f92694210083d64f035013aca87c583a0
lucasuggeri <lucasuggeri@outlook.com>
2022-09-28 21:51:48 -0300

    teste

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from '@shared/errors/appError';
 import '@shared/typeorm';
@@ -11,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
